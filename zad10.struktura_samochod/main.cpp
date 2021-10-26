60f0dbb54b9c4ff94e49559b96b29d21776f040a
Karollson79 <jurzeckarolek@gmail.com>
2021-10-26 08:56:13 +0200

    struktura

---
+++ zad10.struktura_samochod/main.cpp
@@ -0,0 +1,29 @@
+//
+// Created by student on 26.10.21.
+//
+#include "iostream"
+#include "string"
+#include "algorithm"
+#include "map"
+
+using namespace std;
+
+struct samochod{
+    string marka, model, kolor;
+    int rok_produkcji;
+};
+
+void count_elememts(samochod *tab, int no_ele){
+    map <string, int> pom;
+    for(int i = 0; i < no_ele; ++i){
+        ++map[tab[i].marka];
+    }
+    cout << map["Audi"];
+}
+
+int main(){
+    samochod cars[4] = {{"Audi", "A4", "czarny", 2000}, {"Audi", "A7", "bialy", 2009}, {"Audi", "A6", "czarny", 2010}, {"Audi", "A8", "srebrny", 2019}};
+    for(int i = 0; i < 4; ++i)
+        cout << cars[i].marka <<"\t"<< cars[i].model <<"\t" << cars[i].kolor <<"\t\t"<<cars[i].rok_produkcji<< "\n";
+    count_elememts(cars, 4);
+}
