32dc49216f89169aa881d8cdaf763e6f43dfae30
Karollson79 <jurzeckarol@gmail.com>
2021-11-09 14:16:16 +0100

    klasa pojazd

---
+++ zad15.1.konstruktory_kopiujace/include/Pojazd.h
@@ -0,0 +1,8 @@
+//
+// Created by cinamon on 09.11.2021.
+//
+
+#ifndef LIB_CPP_POJAZD_H
+#define LIB_CPP_POJAZD_H
+
+#endif //LIB_CPP_POJAZD_H
---
+++ zad15.1.konstruktory_kopiujace/src/Pojazd.cpp
@@ -0,0 +1,4 @@
+//
+// Created by cinamon on 09.11.2021.
+//
+
